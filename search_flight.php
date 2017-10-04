df406891c94a0ed221942b0d5854937026239224
Orie Chinedu Emmanuel <oriechinedu@gmail.com>
2017-10-04 23:23:26 +0100

    extracted all data from json

--- search_flight.php
+++ search_flight.php
@@ -18,9 +18,26 @@ $client = new Client();
 $response = $client->request('GET', $endPoint, ['verify' => false, 'Content-type' => 'application/json','Accept' => 'application/json']);
  
 $result = $response->getBody()->getContents();
-
-$result = json_decode($result, true);
-
- echo '<pre>';
-var_dump($result);
- echo '</pre>';
+$result = json_decode($result);
+// $data = $result->results[0]->itineraries[0]->outbound->flights[0];
+//  echo '<div>
+//   <p>Departs'.$data->departs_at." Arrives". $data->arrives_at."Origin Airport". $data->origin->airport. "Destination Airport: ".$data->destination->airport.'</p></div>';
+ 
+//  echo '<pre>';
+//  //var_dump($result->results[0]->itineraries[0]->outbound->flights[0]);
+// var_dump($result);
+//  echo '</pre>';
+ echo '<h1>CURRENCY:'.$result->currency.'</h1><hr>';
+ foreach ($result->results as $key => $data) {
+     # code...
+      foreach ($data->itineraries as $key => $iti) {
+           
+        foreach ($iti->outbound->flights as $key => $flight) {
+            # code...
+            echo "<p><b> Departs_at:</b> ". $flight->departs_at.",  <b>Origin:</b>  ". $flight->origin->airport.",  <b>Destination:</b>  ". $flight->destination->airport.
+            ", <b>Arrives_at:</b> ".$flight->arrives_at.",  <b>Flight Number:</b> ".$flight->flight_number. ",  <b>Travel Class:</b> ". $flight->booking_info->travel_class.
+            ",  <b>Seats Remaining:</b> ". $flight->booking_info->seats_remaining."</p>";
+        }
+          # code...
+      }
+ }
